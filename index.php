c514955816328ff3955b55c6b2acf1be6afd8b66
Romain Tello <romain.tello@gmail.com>
2020-03-30 11:38:35 +0200

    Add article view

--- index.php
+++ index.php
@@ -1,116 +1,6 @@
 <?php
 
-class Article {
-    private $id;
-    private $title;
-    private $content;
-    private $cover;
-    private $category;
-
-    public function __construct(
-        $title = '',
-        $content = '',
-        $cover = '',
-        $category = ''
-    ) {
-        $this->title = $title;
-        $this->content = $content;
-        $this->cover = $cover;
-        $this->category = $category;
-    }
-
-    /**
-     * Get the value of id
-     */ 
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Get the value of title
-     */ 
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    /**
-     * Set the value of title
-     *
-     * @return  self
-     */ 
-    public function setTitle($title)
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of content
-     */ 
-    public function getContent()
-    {
-        return $this->content;
-    }
-
-    /**
-     * Set the value of content
-     *
-     * @return  self
-     */ 
-    public function setContent($content)
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of cover
-     */ 
-    public function getCover()
-    {
-        return $this->cover;
-    }
-
-    /**
-     * Set the value of cover
-     *
-     * @return  self
-     */ 
-    public function setCover($cover)
-    {
-        if (!filter_var($cover, FILTER_VALIDATE_URL)) {
-            throw new RuntimeException('Property Article#cover must be a valid url ("' . $cover . '" given).');
-        }
-
-        $this->cover = $cover;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of category
-     */ 
-    public function getCategory()
-    {
-        return $this->category;
-    }
-
-    /**
-     * Set the value of category
-     *
-     * @return  self
-     */ 
-    public function setCategory($category)
-    {
-        $this->category = $category;
-
-        return $this;
-    }
-}
+require_once 'models/Article.php';
 
 // Crée un article à l'aide de son constructeur
 $myArticle = new Article(
@@ -129,4 +19,23 @@ $myArticle
     ->setCover('http://imgs.abduzeedo.com/files/francois/wallpapers/wpw408/abdz_infrared_arashiyama_mockup.jpg')
     ->setCategory('News');
 
-var_dump($myArticle);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= $myArticle->getTitle() ?></h1>
+    <img src="<?= $myArticle->getCover() ?>" />
+    <p>
+        <?= $myArticle->getContent() ?>
+    </p>
+    <div>
+        Published in <?= $myArticle->getCategory() ?>
+    </div>
+</body>
+</html>
---
+++ models/Article.php
@@ -0,0 +1,113 @@
+<?php
+
+class Article {
+    private $id;
+    private $title;
+    private $content;
+    private $cover;
+    private $category;
+
+    public function __construct(
+        $title = '',
+        $content = '',
+        $cover = '',
+        $category = ''
+    ) {
+        $this->title = $title;
+        $this->content = $content;
+        $this->cover = $cover;
+        $this->category = $category;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */ 
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @return  self
+     */ 
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cover
+     */ 
+    public function getCover()
+    {
+        return $this->cover;
+    }
+
+    /**
+     * Set the value of cover
+     *
+     * @return  self
+     */ 
+    public function setCover($cover)
+    {
+        if (!filter_var($cover, FILTER_VALIDATE_URL)) {
+            throw new RuntimeException('Property Article#cover must be a valid url ("' . $cover . '" given).');
+        }
+
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of category
+     */ 
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set the value of category
+     *
+     * @return  self
+     */ 
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
